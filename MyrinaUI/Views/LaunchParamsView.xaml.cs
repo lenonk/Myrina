a1d4a6375a50d8198f28d3a551841a304ecaff60
Lenon <lenon.kitchens@gmail.com>
2019-11-25 07:23:40 -0600

    Moved remaining xaml in MainView.xaml to LaunchParamsView.xaml Fixed image tiling for bar_texture on LogView and InstanceInfoView

---
+++ MyrinaUI/Views/LaunchParamsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyrinaUI.Views {
+    public class LaunchParamsView : UserControl {
+        public LaunchParamsView() {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
